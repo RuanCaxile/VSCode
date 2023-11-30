dd86eab12eb8035215341ac1033a0b75c76bbee6
ruan <ruancaxile@gmail.com>
2023-11-30 10:07:54 -0300

    Evolução das definições de classe,

--- SistemaBancarioPOO/classes.py
+++ SistemaBancarioPOO/classes.py
@@ -1,8 +1,26 @@
+from datetime import date
+
+
 class Cliente:
    def __init__(self, endereco:str, contas:list) -> None:
-      pass  
+      self.endereco = endereco
+      self.contas = contas
+   
+   def realizar_transacao(conta: Conta, transacao: Transacao):
+       pass
+   
+   def adicionar_conta(conta:Conta):
+       pass
 
 
+
+class PessoaFisica(Cliente):
+    def __init__(self, endereco: str, contas: list, cpf: str, nome:str, data_nascimento:date) -> None:
+        super().__init__(endereco, contas)
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+    
 
 
 class Conta:
@@ -16,7 +34,7 @@ class Conta:
     def saldo(self) -> float:
         pass
 
-    def nova_conta(self,cliente:Cliente, numero:int, ):
+    def nova_conta(self,cliente:Cliente, numero:int):
         pass
         
     def sacar(self, valor:float) -> bool:
@@ -25,4 +43,9 @@ class Conta:
     def depositar(self, valor:float) -> bool:
         pass
 
+class ContaCorrente(Conta):
+    def __init__(self, saldo: float, numero: int, agencia: str, cliente: Cliente, historico: Historico, limite:float, limite_saques:int) -> None:
+        super().__init__(saldo, numero, agencia, cliente, historico)
+        self.limite = limite
+        self.limite_saques = limite_saques
 
