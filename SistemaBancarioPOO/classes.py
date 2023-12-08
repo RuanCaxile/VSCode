fd0f83f428144c3c228a7f26f12878d4ccf8a731
ruan <ruancaxile@gmail.com>
2023-12-08 10:49:06 -0300

    mudança em "sucesso_saque"

--- SistemaBancarioPOO/classes.py
+++ SistemaBancarioPOO/classes.py
@@ -134,7 +134,7 @@ class Saque(Transacao):
         return self._valor
 
     def registrar(self, conta):
-        sucesso_saque = conta.sacar(self.valor)
+        sucesso_saque = conta.sacar(self._valor)
 
         if sucesso_saque:
             conta.historico.adicionar_transacao(self)
