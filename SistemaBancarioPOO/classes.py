cc302d512227c2084ef49fee7ef75975ab1a8ec2
ruan <ruancaxile@gmail.com>
2023-12-22 16:27:58 -0300

    versão final

--- SistemaBancarioPOO/classes.py
+++ SistemaBancarioPOO/classes.py
@@ -157,9 +157,7 @@ class Saque(Transacao):
         return self._valor
 
     def registrar(self, conta):
-        print(self.valor)
         sucesso_saque = conta.sacar(self.valor)
-        print(sucesso_saque)
         if sucesso_saque:
             conta.historico.adicionar_transacao(self)
 
